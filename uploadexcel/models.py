56d7c14879204da78b64235c57bdaef82b80b170
quyen300198 <quyenhauik11@gmail.com>
2021-07-25 05:04:38 +0700

    c

--- uploadexcel/models.py
+++ uploadexcel/models.py
@@ -15,8 +15,9 @@ class table_color_fomat(models.Model):
     code_hex = models.CharField(max_length=100)
     code_dali = models.CharField(max_length=100)
     number_img = models.IntegerField() 
+    code_rgb = models.CharField(default=None, blank=True, null=True,max_length=100)
     def as_dict(self):
-        return {'code_hex': self.code_hex, 'code_dali': self.code_dali,'number_img': self.number_img}
+        return {'code_hex': self.code_hex, 'code_dali': self.code_dali,'number_img': self.number_img,'code_rgb': self.code_rgb}
 
 class ImageObj(models.Model):
     caption=models.CharField(max_length=100)
